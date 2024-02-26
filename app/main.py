7e8514885531f5714ddb424bb704c1259cf35350
Mehrnaz <141185685+Mehrnaz4Hosseini@users.noreply.github.com>
2024-02-26 21:33:25 +0330

    add blog router

--- app/main.py
+++ app/main.py
@@ -3,8 +3,12 @@ from typing import Optional, Union
 from pydantic import BaseModel
 import uvicorn
 from enum import Enum
+from app.routers import blog
 
 app = FastAPI()  #instance
+
+
+app.include_router(blog.router)
 
 
 @app.get("/")   #HTTP "methods" -> POST  GET  PUT  DELETE
@@ -23,38 +27,6 @@ def read_root():# is called <<Path operation function>>
 @app.get("/about")   #about path
 def about():
     return {'data': {'This is about page.'}}
-
-
-@app.get('/blog/unpublished')
-def get_unpublished():
-    return {'data': 'all unpublished data'}
-
-
-@app.get('/blog/{id}')   # usage -> creating routing
-def get_id(id: int):
-    # fetch blog with id = id 
-    # fetch -> API Requests: In the context of web development, “fetch” is often used to refer to the process of
-    # requesting data from an API.
-    return {'data': id}
-
-# Query parameter -> 1. not in the path 
-#                    2. Default value
-#                    3. Optional[ <type> ] = None
-@app.get('/blog')
-
-def my_query(limit = 10, published : bool = False, sort: Union[str, None] = None):
-    if published:
-        return {'data': {'limit': f'Hey you got {limit} published blogs from database.', 'published': published}}
-    else:
-        return {'data':{'limit': "Sorry, you just got a few blogs", 'published': published}}
-
-
-# if the name in in path -> path parameter
-# else                   -> query parameter
-
-@app.get('/blog/{id}/comment')
-def comment(id, limit = 10):
-    return{'data':{id: {'1,2,3'}, 'limit': limit}}
 
 
 @app.get("/models")
